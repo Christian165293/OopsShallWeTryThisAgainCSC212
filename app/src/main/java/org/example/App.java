0728eb5f5e56f3dc37f7b930cc4865528b6f5258
Christian165293 <christian.bourquin05@gmail.com>
2025-01-24 22:51:50 -0600

    Code is complete however its throwing errors when I run it in the terminal so im going to figure out why that's happening and come back to fix it.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,26 @@
 package org.example;
 
-public class App {
-  public int getInput() {
-    // Implement your function here
-  }
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // Test your function here in a driver program
-  }
+public class App {
+    public int getInput(int lowerBound, int upperBound, String userPrompt, String errorMessage) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(userPrompt);
+        int userInput = scanner.nextInt();
+
+        while (userInput < lowerBound || userInput > upperBound) {
+            System.out.println(errorMessage);
+            System.out.println(userPrompt);
+            userInput = scanner.nextInt();
+        }
+
+        return userInput;
+    }
+
+    public static void main(String[] args) {
+
+        App app1 = new App();
+        int userInput = app1.getInput(10, 50, "Please enter an integer value:", "That input is invalid.");
+        System.out.println("You entered: " + userInput);
+    }
 }
